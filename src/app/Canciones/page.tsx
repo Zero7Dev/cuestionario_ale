dc47cebf475997144a6d0b7291e2d1682d032594
JulioCV <zero13dev@gmail.com>
2024-09-29 11:45:08 -0500

    Re-initialized repository and added existing changes2ww

--- src/app/Canciones/page.tsx
+++ src/app/Canciones/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react';
 
 const CancionesPage = () => {
+
+  useEffect(() => {
+    // Lanza un error para redirigir a la página de error
+    const error = new Error("Algo salió mal en CancionesPage");
+    throw error;
+  }, []);
+
   return (
-    <div className='text-white font-bold'>loading... 
-
+    <div className='text-white font-bold'>
+      loading...
     </div>
-  )
-}
+  );
+};
 
-export default CancionesPage
+export default CancionesPage;
--- src/app/Nuestros-Planes/page.tsx
+++ src/app/Nuestros-Planes/page.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 
 const NuestrosPlanes = () => {
+  useEffect(() => {
+    // Lanza un error para redirigir a la página de error
+    const error = new Error("Algo salió mal en CancionesPage");
+    throw error;
+  }, []);
   return (
     <div className='text-white font-bold'>loading... 
 
