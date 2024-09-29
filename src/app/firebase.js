9518771a346a65302434dc5570ec8b187bf8f544
JulioCV <zero13dev@gmail.com>
2024-09-29 02:33:40 -0500

    x

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,31 +1,27 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore/lite";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDX5R0FEXEXVmieVwBvSnS3amsb7chFZa0",
-  authDomain: "amor-ed8f1.firebaseapp.com",
-  projectId: "amor-ed8f1",
-  storageBucket: "amor-ed8f1.appspot.com",
-  messagingSenderId: "703006984568",
-  appId: "1:703006984568:web:92ab4ad7bc89a4bfb1608d",
-  measurementId: "G-CVJQ9GSK9G"
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+let app; // Declarar app fuera de la función
+
+if (typeof window !== "undefined") { // Verificar si estamos en el cliente
+  // Tu configuración de Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyDX5R0FEXEXVmieVwBvSnS3amsb7chFZa0",
+    authDomain: "amor-ed8f1.firebaseapp.com",
+    projectId: "amor-ed8f1",
+    storageBucket: "amor-ed8f1.appspot.com",
+    messagingSenderId: "703006984568",
+    appId: "1:703006984568:web:92ab4ad7bc89a4bfb1608d",
+    measurementId: "G-CVJQ9GSK9G"
+  };
+  
 
 
+  // Inicializa Firebase
+  app = initializeApp(firebaseConfig);
+}
 
-// Inicializa Firebase
-const apps = initializeApp(firebaseConfig);
-
-// Inicializa Firestore
-const db = getFirestore(apps);
+const db = app ? getFirestore(app) : null; // Solo obtener Firestore si app fue inicializada
 
 export { db };
